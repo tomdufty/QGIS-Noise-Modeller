8d1ef753d837db5bf8ab62023a706471d5b09565
Tom Dufty <tom.dufty@hotmail.com>
2017-08-26 11:38:17 +1000

    initial classes

--- pyBatchENM.py
+++ pyBatchENM.py
@@ -2,11 +2,28 @@ class ENMrun:
     def __init__(self,source,section):
         self.source=source
         self.section=section
+
     def start_run(self):
         print('running')
 
-class source_file:
+    def read_results(self):
+        print('reading results')
+
+class SourceFile:
     path = ''
 
     def __init__(self):
         print('new source file')
+
+    def write(self):
+        print('writing source file')
+
+
+class SectionFile:
+    path = ''
+
+    def __init__(self):
+        print('new section file')
+
+    def write(self):
+        print('writing section file')
