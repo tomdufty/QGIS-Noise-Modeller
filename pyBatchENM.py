8bd9316102440076dd76f501838df29eeb96f8a3
Tom Dufty <tom.dufty@hotmail.com>
2017-08-26 12:14:41 +1000

    added sources,recievers, met cond classes

--- pyBatchENM.py
+++ pyBatchENM.py
@@ -1,4 +1,7 @@
 class ENMrun:
+
+    ENMpath=''
+
     def __init__(self,source,section):
         self.source=source
         self.section=section
@@ -8,6 +11,7 @@ class ENMrun:
 
     def read_results(self):
         print('reading results')
+
 
 class SourceFile:
     path = ''
@@ -27,3 +31,50 @@ class SectionFile:
 
     def write(self):
         print('writing section file')
+
+class ScenarioFile:
+    path = ''
+
+    def __init__(self):
+        print('new scenario file')
+
+    def write(self):
+        print('writing scenario file')
+
+
+class Reciever:
+    def __init__(self,x,y,z,h):
+        print('new receiver')
+        self.x=x
+        self.y=y
+        self.z=z
+        self.h=h
+
+class Spectrum:
+    def __init__(self):
+        print('new spectrum')
+
+class Source:
+    def __init__(self,x,y,z,h):
+        print('new source')
+        self.x=x
+        self.y=y
+        self.z=z
+        self.h=h
+
+class Section:
+    def __init__(self,source):
+        print('new section')
+        self.source=source
+        self.xzPointList=[]
+
+    def add_point(self,x,z):
+        self.xzPointList.append(self,x,z)
+
+class MetCond:
+    def __init__(self,temp,humidity,wind_speed,wind_dir,temp_grad):
+        self.temp=temp
+        self.humidity=humidity
+        self.wind_speed=wind_speed
+        self.wind_dir=wind_dir
+        self.temp_grad=temp_grad
