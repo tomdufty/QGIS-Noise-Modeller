1c398c1fab2a707ad4d1811b1681a3dd05b23102
Tom Dufty <tom.dufty@hotmail.com>
2017-08-27 17:57:23 +1000

    successful creating of results table

--- pyBatchENM.py
+++ pyBatchENM.py
@@ -97,13 +97,71 @@ class MetCond:
         self.wind_dir=wind_dir
         self.temp_grad=temp_grad
 
-#start of main code
-sourcefiletemp=SourceFile()
-sectionFileTemp=SectionFile()
-testRun=ENMrun(sectionFileTemp,sectionFileTemp)
-testRun.start_run()
-testRun.read_results()
 
-#
+class ResultTable:
+    sql_create_results_table = """ CREATE TABLE IF NOT EXISTS results (
+                                            id integer PRIMARY KEY,
+                                            RecNo integer NOT NULL,
+                                            SourceNo integer NOT NULL,
+                                            MetCondNo integer NOT NULL,
+                                            speed FLOAT(5),
+                                            direction FLOAT(5),
+                                            r12_5Hz FLOAT(5),
+                                            r16Hz float(5),
+                                            r20Hz float(5),
+                                            r25Hz float(5),
+                                            r31_5Hz float(5),
+                                            r40Hz float(5),
+                                            r50Hz float(5),
+                                            r63Hz float(5),
+                                            r80Hz float(5),
+                                            r100Hz float(5),
+                                            r125Hz float(5),
+                                            r160Hz float(5),
+                                            r200Hz float(5),
+                                            r250Hz float(5),
+                                            r315Hz float(5),
+                                            r400Hz float(5),
+                                            r500Hz float(5),
+                                            r630Hz float(5),
+                                            r800Hz float(5),
+                                            r1kHz float(5),
+                                            r1_25kHz float(5),
+                                            r1_6kHz float(5),
+                                            r2kHz float(5),
+                                            r2_5kHz float(5),
+                                            r3_15kHz float(5),
+                                            r4kHz float(5),
+                                            r5kHz float(5),
+                                            r6_3kHz float(5),
+                                            r8kHz float(5),
+                                            r10kHz float(5),
+                                            r12_5kHz float(5),
+                                            r16kHz float(5),
+                                            r20kHz float(5)
+                                        ); """
 
-results_connection=sqlite3.connect()
+    def __init__(self,result_path):
+        try:
+            conn = sqlite3.connect(result_path)
+        except Exception as e:
+            print(e)
+        if conn is not None:
+            try:
+               c = conn.cursor()
+               print("create table")
+               c.execute(self.sql_create_results_table,())
+               print("executed")
+            except Exception as e:
+                print("exception")
+                print(e)
+
+# start of main code
+# sourcefiletemp=SourceFile()
+# sectionFileTemp=SectionFile()
+# testRun=ENMrun(sectionFileTemp,sectionFileTemp)
+# testRun.start_run()
+# testRun.read_results()
+
+results_path = "results_test.db"
+results_table = ResultTable(results_path)
